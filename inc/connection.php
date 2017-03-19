dcab5ee61e42ead155ac1f7b510693af0beb838e
tobibello <bello.tobi@outlook.com>
2017-03-19 08:35:22 +0100

    initial commit

---
+++ inc/connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "B3d0fSt0n3";
+$dbname = "psams_db";
+try {
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    exit;
+}
