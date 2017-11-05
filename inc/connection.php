1f015fa0a3d26f8d86d1fc24847b72b53a75697a
Tobi Bello <bello.tobi@outlook.com>
2017-11-05 13:59:35 +0100

    database update

--- inc/connection.php
+++ inc/connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "B3d0fSt0n3";
-$dbname = "db_psams";
+$password = "";
+$dbname = "psams";
 try {
     $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
